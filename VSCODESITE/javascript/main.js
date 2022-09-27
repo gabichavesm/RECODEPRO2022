df3454f9f3d81cffb1e590ae81b7504db27d1b27
Gabriela Chaves <101366585+gabichavesm@users.noreply.github.com>
2022-09-26 23:10:51 -0300

    Add files via upload

---
+++ VSCODESITE/javascript/main.js
@@ -0,0 +1,19 @@
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// div 2
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
